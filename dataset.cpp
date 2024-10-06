a2acc2769ebd1fb855b89903df4fae268cf23894
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-06 12:05:05 +0200

    Update dataset.cpp

--- dataset.cpp
+++ dataset.cpp
@@ -1,55 +1,33 @@
-/*
-Author: David Holmqvist <daae19@student.bth.se>
-*/
+#include "dataset.h"
 
-#include "dataset.hpp"
-#include "vector.hpp"
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <ctype.h>
-
-void Dataset::Read(const std::string& filename, )
+void Read(const std::string &filename, std::array<Vector, 1024> &matrix)
 {
-
-    //save into a 2d array instead of vector
     std::ifstream file;
     file.open(filename);
 
     unsigned dimension;
     file >> dimension;
-    Vector result[dimension]; //may need to use 1024
 
-    file.seekg(1, std::ios::cur); //skip /n
+    file.seekg(1, std::ios::cur);
 
     for(unsigned i = 0; i < dimension; ++i)
     {
-        Vector newVec[dimension];
-        //double temp[1024]; //change to newVec
-        for(unsigned x = 0; x < dimension; ++x)
-            file >> newVec[x];
-
-        result[i] = newVec;
+        double temp;
+        file >> temp;
+        matrix[i].Add(temp);
     }
 }
 
-void write(std::vector<double> data, std::string filename)
+
+
+
+
+
+void Write(const std::string& filename, std::array<std::array<double, 1024>, 1024>& matrix)
 {
     std::ofstream f {};
 
     f.open(filename);
 
-    //try catch?
-    //asyncio if we are writing at the same time? as we generate data, for 1 thread?
-    if (!f) {
-        std::cerr << "Failed to write data to file " << filename << std::endl;
-        return;
-    }
-
-    for (auto i { 0 }; i < data.size(); i++) {
-        f << std::setprecision(std::numeric_limits<double>::digits10 + 1) << data[i] << std::endl;
-    }
 }
 
-};
