df959a19b997ce8645f27baa36dfa149a77d5789
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-07 13:56:13 +0200

    Update analysis.cpp

--- analysis.cpp
+++ analysis.cpp
@@ -1,20 +1,19 @@
 #include "analysis.h"
 
-int Correlation_coefficients(std::array<Vector, 1024> &matrix, std::array<double, 523776>& data)
+int Correlation_coefficients(std::array<Vector, 512> &matrix, std::array<double, 130816>& data)
 {
-    int dimension = 1024;
+    int dimension = 128;
     int index = 0;
     for(unsigned i = 0; i < dimension-1; ++i)
-    {
         for(unsigned j = (i+1); j < dimension; ++j)
             data[index++] = Pearson(matrix[i], matrix[j]);
-    }
 
     return index;
 }
 
-double Pearson(Vector& vec1, Vector& vec2)
+double Pearson(Vector vec1, Vector vec2)
 {
+    // todo: if time: for small arrays we have to copy the full 512 array even if we only use 128 positions
     double xMean = vec1.Mean();
     double yMean = vec2.Mean();
 
@@ -24,13 +23,10 @@ double Pearson(Vector& vec1, Vector& vec2)
     double xMag = vec1.Magnitude();
     double yMag = vec2.Magnitude();
 
-    
+    vec1 / xMag;
+    vec2 / yMag;
 
-    auto x_mm_over_x_mag { x_mm / x_mag };
-    auto y_mm_over_y_mag { y_mm / y_mag };
-
-    auto r { x_mm_over_x_mag.dot(y_mm_over_y_mag) };
+    double r = vec1.DotProduct(vec2);
 
     return std::max(std::min(r, 1.0), -1.0);
 }
-
