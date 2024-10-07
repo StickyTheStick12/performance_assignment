7b1192c1c542f1acd5ec3a710a77884fb2e2e34f
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-07 15:02:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,28 @@
-#include <array>
+#include "analysis.h"
 #include "vector.h"
 #include "dataset.h"
 
 //TODO
-// 1: check if we can use 512 bit instructions for vector, then remove 256 bit (check alignment (must be 64))
-// 2: increase stack in main
-// 3: fix vector division
-// 4: fix vector dot product
-// 5: fix write to file from dataset
-// 6: test run
-// 7: maybe align all arrays?
-// 8: general code cleanup
-// 9: fix multithreading
-// 10: general code cleanup
+// 1: fix 1024 dataset
+// 2: fix so we use dimension everywhere and not hardcoded
+// 3: general code cleanup
+// 4: fix multithreading
+// 5: general code cleanup
 
 
 
 
-int main(int argc, char const* argv[]) {
-    //remove this during runtime
-    //if (argc != 3) {
-    //    std::cerr << "Usage: " << argv[0] << " [dataset] [outfile]" << std::endl;
-    //    std::exit(1);
-    //}
+int main(int argc, char const* argv[])
+{
+    std::array<Vector128, 512> matrix;
 
+    Read("128.data", matrix);
 
-    // increase stack
+    std::array<double, 130816> data;
 
-    std::array<Vector, 1024> matrix;
+    int index = Correlation_coefficients(matrix, data);
 
-    Read(argv[1], matrix);
-
-
-
-
-    //auto corrs { Analysis::correlation_coefficients(datasets) };
-    //Dataset::write(corrs, argv[2]);
+    Write("out.txt", data, index);
 
     return 0;
 }
