10311524eb0a9cfee330e66532a750d83474701f
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-07 18:55:00 +0200

    Update vector.h

--- vector.h
+++ vector.h
@@ -64,20 +64,4 @@ public:
     double DotProduct(const Vector1024& other) const;
 };
 
-
-class Vector
-{
-    unsigned size = 0;
-    double sum = 0;
-
-public:
-    alignas(32) double data[128];
-    void Add(double num);
-    double Mean() const;
-    void operator-(double sub);
-    void operator/(double divisor);
-    double Magnitude() const;
-    double DotProduct(const Vector& other) const;
-};
-
 #endif
