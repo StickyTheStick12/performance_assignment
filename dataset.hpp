73a5dd1d0e00b226fbc18fce281ac0a2c089a031
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-07 13:56:55 +0200

    Update dataset.hpp

--- dataset.hpp
+++ dataset.hpp
@@ -3,9 +3,15 @@
 
 #include <fstream>
 #include <array>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "vector.h"
+#include <iomanip>
 
-void Read(const std::string& filename, std::array<Vector, 1024>& matrix);
-void Write(const std::string& filename, std::array<std::array<double, 1024>, 1024>& matrix);
+
+void Read(const std::string& filename, std::array<Vector, 512>& matrix);
+void Write(const std::string& filename, std::array<double, 130816>& data, int endPoint);
+double CharArrToDouble(const char* arr);
 
 #endif
