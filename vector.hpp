b4acf370f794fb9b683dcb34839244bbffe5dfc2
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-07 15:03:04 +0200

    Update vector.hpp

--- vector.hpp
+++ vector.hpp
@@ -4,13 +4,74 @@
 #include "immintrin.h"
 #include <cmath>
 
-class Vector
+class Vector128
+{
+    unsigned size = 0;
+    double sum = 0;
+
+public:
+    alignas(32) double data[128];
+    void Add(double num);
+    double Mean() const;
+    void operator-(double sub);
+    void operator/(double divisor);
+    double Magnitude() const;
+    double DotProduct(const Vector128& other) const;
+};
+
+class Vector256
+{
+    unsigned size = 0;
+    double sum = 0;
+
+public:
+    alignas(32) double data[256];
+    void Add(double num);
+    double Mean() const;
+    void operator-(double sub);
+    void operator/(double divisor);
+    double Magnitude() const;
+    double DotProduct(const Vector256& other) const;
+};
+
+class Vector512
 {
     unsigned size = 0;
     double sum = 0;
 
 public:
     alignas(32) double data[512];
+    void Add(double num);
+    double Mean() const;
+    void operator-(double sub);
+    void operator/(double divisor);
+    double Magnitude() const;
+    double DotProduct(const Vector512& other) const;
+};
+
+class Vector1024
+{
+    unsigned size = 0;
+    double sum = 0;
+
+public:
+    alignas(32) double data[1024];
+    void Add(double num);
+    double Mean() const;
+    void operator-(double sub);
+    void operator/(double divisor);
+    double Magnitude() const;
+    double DotProduct(const Vector1024& other) const;
+};
+
+
+class Vector
+{
+    unsigned size = 0;
+    double sum = 0;
+
+public:
+    alignas(32) double data[128];
     void Add(double num);
     double Mean() const;
     void operator-(double sub);
