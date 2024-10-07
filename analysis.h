f91bea8a71ca2edc43076ce768c585330eea07d0
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-07 18:53:03 +0200

    Update analysis.h

--- analysis.h
+++ analysis.h
@@ -7,7 +7,7 @@
 void CorrelationCoefficients128(std::array<Vector128, 128> &matrix, std::array<double, 8128>& data);
 void CorrelationCoefficients256(std::array<Vector256, 256> &matrix, std::array<double, 32640>& data);
 void CorrelationCoefficients512(std::array<Vector512, 512> &matrix, std::array<double, 130816>& data);
-void CorrelationCoefficients1024(std::array<Vector1024, 512> &matrix, std::array<double, 130816>& data);
+int CorrelationCoefficients1024(std::array<Vector1024, 512> &matrix, double* data, int index=0);
 
 
 double Pearson128(Vector128 vec1, Vector128 vec2);
