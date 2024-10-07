c8c3fa9cd7c3f2db7c12587392be4dc44821c37e
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-07 13:56:32 +0200

    Update analysis.hpp

--- analysis.hpp
+++ analysis.hpp
@@ -4,7 +4,7 @@
 #include <array>
 #include "vector.h"
 
-int Correlation_coefficients(std::array<Vector, 1024> &matrix, std::array<double, 523776>& data);
-double Pearson(Vector& vec1, Vector& vec2);
+int Correlation_coefficients(std::array<Vector, 512> &matrix, std::array<double, 130816>& data);
+double Pearson(Vector vec1, Vector vec2);
 
 #endif
