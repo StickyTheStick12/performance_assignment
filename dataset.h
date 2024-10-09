58e0e06d17f63de74d5adbc71b7f1639f77e924a
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-09 11:48:04 +0200

    Update dataset.h

--- dataset.h
+++ dataset.h
@@ -1,13 +1,12 @@
 #ifndef DATASET_H
 #define DATASET_H
 
-#include <fstream>
 #include <array>
 #include <sys/mman.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include "vector.h"
-#include <iomanip>
+
+void WriteThreaded(char* mappedData, double data, int index);
 
 void Write128(const std::string& filename, std::array<double, 8128>& data);
 void Write256(const std::string& filename, std::array<double, 32640>& data);
