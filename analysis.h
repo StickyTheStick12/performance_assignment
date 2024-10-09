be977cf27c02bba10140596abd46f9be7b9445d4
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-09 15:34:58 +0200

    Update analysis.h

--- analysis.h
+++ analysis.h
@@ -13,7 +13,7 @@ int CorrelationCoefficients1024(std::array<Vector1024, 512> &matrix, double* dat
 void CorrelationCoefficients128Threaded(std::array<Vector128, 128> &matrix, std::array<double, 8128>& data);
 void CorrelationCoefficients256Threaded(std::array<Vector256, 256> &matrix, std::array<double, 32640>& data);
 void CorrelationCoefficients512Threaded(std::array<Vector512, 512> &matrix, std::array<double, 130816>& data);
-int CorrelationCoefficients1024Threaded(std::array<Vector1024, 512> &matrix, double* data, int index=0);
+int CorrelationCoefficients1024Threaded(std::array<Vector1024, 512> &matrix, double* data);
 
 double Pearson128(Vector128 vec1, Vector128 vec2);
 double Pearson256(Vector256 vec1, Vector256 vec2);
