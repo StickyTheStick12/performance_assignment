50a97635ff156ab2caadaf70d09d8422fff43a2f
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-10 15:50:26 +0200

    Update analysis.h

--- analysis.h
+++ analysis.h
@@ -8,13 +8,11 @@
 void CorrelationCoefficients128(std::array<Vector128, 128> &matrix, std::array<double, 8128>& data);
 void CorrelationCoefficients256(std::array<Vector256, 256> &matrix, std::array<double, 32640>& data);
 void CorrelationCoefficients512(std::array<Vector512, 512> &matrix, std::array<double, 130816>& data);
-int CorrelationCoefficients1024(std::array<Vector1024, 512> &matrix, double* data, int index=0);
+void CorrelationCoefficients1024(Vector1024* matrix, std::array<double, 523776>& data);
 
 void CorrelationCoefficients128Threaded(std::array<Vector128, 128> &matrix, std::array<double, 8128>& data);
 void CorrelationCoefficients256Threaded(std::array<Vector256, 256> &matrix, std::array<double, 32640>& data);
 void CorrelationCoefficients512Threaded(std::array<Vector512, 512> &matrix, std::array<double, 130816>& data);
-int CorrelationCoefficients1024Threaded(std::array<Vector1024, 512> &matrix, double* data);
-
-double Pearson1024(Vector1024 vec1, Vector1024 vec2);
+void CorrelationCoefficients1024Threaded(Vector1024* matrix, std::array<double, 523776>& data);
 
 #endif
