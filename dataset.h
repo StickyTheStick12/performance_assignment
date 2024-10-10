c4ee2a77b830affcaa1c46d0adfa497a85758143
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-10 15:50:44 +0200

    Update dataset.h

--- dataset.h
+++ dataset.h
@@ -12,6 +12,6 @@ void WriteThreaded(char* mappedData, double data, int index);
 void Write128(const std::string& filename, std::array<double, 8128>& data);
 void Write256(const std::string& filename, std::array<double, 32640>& data);
 void Write512(const std::string& filename, std::array<double, 130816>& data);
-void Write1024(const std::string& filename, double* data);
+void Write1024(const std::string& filename, std::array<double, 523776>& data);
 
 #endif
