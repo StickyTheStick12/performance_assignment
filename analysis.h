7e013eea374e66a51c300ffc924146c546ada55b
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-12 15:41:40 +0200

    Update analysis.h

--- analysis.h
+++ analysis.h
@@ -3,16 +3,15 @@
 
 #include <array>
 #include "vector.h"
-#include <atomic>
 
 void CorrelationCoefficients128(std::array<Vector128, 128> &matrix, std::array<double, 8128>& data);
 void CorrelationCoefficients256(std::array<Vector256, 256> &matrix, std::array<double, 32640>& data);
 void CorrelationCoefficients512(std::array<Vector512, 512> &matrix, std::array<double, 130816>& data);
 void CorrelationCoefficients1024(Vector1024* matrix, std::array<double, 523776>& data);
 
-void CorrelationCoefficients128Threaded(std::array<Vector128, 128> &matrix, std::array<double, 8128>& data);
-void CorrelationCoefficients256Threaded(std::array<Vector256, 256> &matrix, std::array<double, 32640>& data);
-void CorrelationCoefficients512Threaded(std::array<Vector512, 512> &matrix, std::array<double, 130816>& data);
-void CorrelationCoefficients1024Threaded(Vector1024* matrix, std::array<double, 523776>& data);
+void CorrelationCoefficients128Threaded(std::array<Vector128, 128> &matrix, std::array<double, 8128>& data, int startPoint, int endPoint);
+void CorrelationCoefficients256Threaded(std::array<Vector256, 256> &matrix, std::array<double, 32640>& data, int startPoint, int endPoint);
+void CorrelationCoefficients512Threaded(std::array<Vector512, 512> &matrix, std::array<double, 130816>& data, int startPoint, int endPoint);
+void CorrelationCoefficients1024Threaded(Vector1024* matrix, std::array<double, 523776>& data, int startPoint, int endPoint);
 
 #endif
